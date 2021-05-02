27ee990ff4f2f0d4792c078dcfb22d7f4a5eab5a
billythegoat356 <77754159+billythegoat356@users.noreply.github.com>
2021-05-02 22:13:39 +0200

    Create file_corruptor.py

---
+++ file_corruptor.py
@@ -0,0 +1,36 @@
+from os import rename
+
+"""
+1 KiloOctet (ko) = 1 000 octets
+1 MégaOctet (Mo) = 1 000 000 octets
+1 GigaOctet (Go) = 1 000 000 000 octets
+"""
+
+caractere = "ç"
+ecriture = ""
+
+print("Quel type de fichier voulez-vous corrompre?\n")
+
+type_de_fichier = input("fichier.")
+
+print("""\nQuelle taille voulez-vous que le fichier aie?
+
+Exemple : 
+1 KiloOctet (ko) = 1 000 octets
+1 MégaOctet (Mo) = 1 000 000 octets
+1 GigaOctet (Go) = 1 000 000 000 octets
+
+""")
+taille_du_fichier = input("Taille [en octets]: ")
+
+for i in range(int(taille_du_fichier)):
+    ecriture += caractere
+
+with open('fichier.txt', "w") as f :
+    f.write(ecriture)
+    f.close()
+
+rename('fichier.txt', f'fichier.{type_de_fichier}')
+
+input("\nFichier créé ;)")
+
